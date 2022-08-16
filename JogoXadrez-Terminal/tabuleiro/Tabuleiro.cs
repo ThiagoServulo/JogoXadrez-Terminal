43c5523729af80c8251e15557494726c4be472de
ThiagoServulo <thiago.servulo@sga.pucminas.br>
2022-08-16 11:20:45 -0300

    Criação da Exceção personalizada para o tabuleiro

--- JogoXadrez-Terminal/tabuleiro/Tabuleiro.cs
+++ JogoXadrez-Terminal/tabuleiro/Tabuleiro.cs
@@ -18,10 +18,38 @@
             return pecas[linha, coluna];
         }
 
+        public Peca AcessarPeca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
         public void ColocarPeca(Peca peca, Posicao pos)
         {
             pecas[pos.Linha, pos.Coluna] = peca;
             peca.Pos = pos;
+        }
+
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return AcessarPeca(pos) != null;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if(!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida!");
+            }
         }
     }
 }
